cfb8fc1288db3530360d13a04737d51e5617c4bd
Nicolay Mena <nicolay.mena@gmail.com>
2023-09-08 09:02:52 +0200

    ms commit

---
+++ Servicios.api.Seguridad/Controllers/UsuarioController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Servicios.api.Seguridad.Core.Application;
+using Servicios.api.Seguridad.Core.Dto;
+
+namespace Servicios.api.Seguridad.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+
+        private readonly IMediator _mediator;
+
+        public UsuarioController(IMediator mediator) {
+            _mediator = mediator;
+        }
+
+        [HttpPost("registrar")]
+        public async Task<ActionResult<UsuarioDto>> Registrar(Register.UsuarioRegisterCommand parametros) {
+            return await _mediator.Send(parametros);
+        }
+    }
+}
---
+++ Servicios.api.Seguridad/Startup.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using FluentValidation.AspNetCore;
+using MediatR;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Servicios.api.Seguridad.Core.Application;
+using Servicios.api.Seguridad.Core.Entities;
+using Servicios.api.Seguridad.Core.Persistence;
+
+namespace Servicios.api.Seguridad
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers().AddFluentValidation(x => x.RegisterValidatorsFromAssemblyContaining<Register>());
+
+            services.AddDbContext<SeguridadContexto>(opt =>
+            {
+                opt.UseSqlServer(Configuration.GetConnectionString("ConexionDB"));
+            });
+
+            var builder = services.AddIdentityCore<Usuario>();
+            var identityBuilder = new IdentityBuilder(builder.UserType, builder.Services);
+            identityBuilder.AddEntityFrameworkStores<SeguridadContexto>();
+            identityBuilder.AddSignInManager<SignInManager<Usuario>>();
+            services.TryAddSingleton<ISystemClock, SystemClock>();
+
+
+            services.AddMediatR(typeof(Register.UsuarioRegisterCommand).Assembly);
+
+            services.AddAutoMapper(typeof(Register.UsuarioRegisterHandler));
+
+
+            
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
